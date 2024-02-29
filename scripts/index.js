5233ad28758a28fb9d64b74e0e817d42ecb5b63e
Natalia Shebekina <lady.malfoy1995@yandex.ru>
2024-02-29 10:24:16 +0300

    Sprint 6 fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,30 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const content = document.querySelector('.places__list')
+const content = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
-initialCards.forEach(function (item) {
+// @todo: Функция создания карточкиc
+initialCards.forEach(initialCards => {
+  const card = addCard(initialCards.link, initialCards.name);
+
+  content.prepend(card);
+
+});
+
+function addCard(imgLink, imgName) {
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const altTxt = imgLink;
 
-  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').src = imgLink;
+  cardElement.querySelector('.card__image').alt = altTxt;
   // cardElement.querySelector('.card__description').textContent = descriptionValue;
-  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__title').textContent = imgName;
 
   const delBtn = cardElement.querySelector('.card__delete-button');
   delBtn.addEventListener('click',delCard);
 
-  content.prepend(cardElement);
-
-});
+  return cardElement;
+}
 
 /*просто для проверки работоспособности*/
 const addButton = document.querySelector('.profile__add-button');
